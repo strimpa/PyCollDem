08c522b634c10e251c0706c785121297b7a81ebc
gunnar <mail@gunnardroege.de>
2014-08-17 17:17:16 +0100

    commit changes to go back to stash

--- CollDem/views.py
+++ CollDem/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from CollDem.forms import LogOnForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import AnonymousUser
+from django.template import RequestContext
+from django.http import HttpResponse
 
-def home():
-	return render('home.html')
+def home(request):
+	logon_form = LogOnForm()
+	entermsg_form
+	user = AnonymousUser()
+
+	if request.method=='POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		if user is not None and user.is_active:
+			login(request, user)
+		else:
+			logon_form = LogOnForm(data=request.POST)
+
+	return render(request, 'home.html', {'logon_form':logon_form, 'user':user}, context_instance=RequestContext(request))
--- PyCollDem/urls.py
+++ PyCollDem/urls.py
@@ -5,8 +5,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'PyCollDem.views.home', name='home'),
-    url(r'^messages/(P<id>\d*)', 'PyCollDem.messageViews.messages'),
+    url(r'^$', 'CollDem.views.home', name='home'),
+    url(r'^messages/(P<id>\d*)', 'CollDem.messageViews.messages'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
